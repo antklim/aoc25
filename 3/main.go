3a45db257d4371d868cb46fdb4705a331e2096ee
antklim <antklim@gmail.com>
2025-12-04 11:28:09 +1100

    day3: part 1

---
+++ 3/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unicode"
+
+	"github.com/antklim/aoc25/internal/utils"
+)
+
+func main() {
+	joltages, err := utils.ReadInput("input.txt", mapJoltage)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read input: %v", err)
+		os.Exit(1)
+	}
+
+	maxJoltages := make([]int, 0, len(joltages))
+	for _, j := range joltages {
+		maxJoltages = append(maxJoltages, maxJoltage(j))
+	}
+
+	sum := 0
+	for _, j := range maxJoltages {
+		sum += j
+	}
+
+	fmt.Printf("total joltages: %d\n", sum)
+	os.Exit(0)
+}
+
+func mapJoltage(s string) ([]int, error) {
+	result := make([]int, 0, len(s))
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return nil, fmt.Errorf("invalid input: %c is not a digit", c)
+		}
+		result = append(result, int(c-'0'))
+	}
+	return result, nil
+}
+
+func maxJoltage(a []int) int {
+	var jolts []int
+
+	for i, d := range a[:len(a)-1] {
+		for _, v := range a[i+1:] {
+			jolts = append(jolts, d*10+v)
+		}
+	}
+
+	return max(jolts)
+}
+
+func max(a []int) int {
+	m := a[0]
+	for _, v := range a[1:] {
+		if v > m {
+			m = v
+		}
+	}
+	return m
+}
---
+++ 3/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestMaxJoltage(t *testing.T) {
+	testCases := []struct {
+		a        []int
+		expected int
+	}{
+		{
+			a:        []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1},
+			expected: 98,
+		},
+		{
+			a:        []int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9},
+			expected: 89,
+		},
+		{
+			a:        []int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8},
+			expected: 78,
+		},
+		{
+			a:        []int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1},
+			expected: 92,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := maxJoltage(tc.a)
+		if got != tc.expected {
+			t.Errorf("maxJoltage(%v) want %d, got %d", tc.a, tc.expected, got)
+		}
+	}
+}
